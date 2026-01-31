5c2af211dceab97b497c40ef6f9910ea38932cd9
StanleyXie <stanley.xie@outlook.com>
2026-01-31 14:55:07 +0100

    feat: flip banner layout and refine T-shape icon #patch

--- src/banner.rs
+++ src/banner.rs
@@ -1,39 +1,39 @@
 use console::style;
 
 pub fn print_banner() {
-    let logo = [
-        "      .-------.      ",
-        "    /    |    \\     ",
-        "   |_____|_____|    ",
-        "   |  |  #  |  |    ",
-        "   |  |  #  |  |    ",
-        "   |  |  #  |  |    ",
-        "   |  |__|__|  |    ",
-        "    \\    |    /     ",
-        "      '-----'       ",
+    let text = [
+        r#"  ____  ____  _      ____  "#,
+        r#" / ___)(_  _)( \    / __ \ "#,
+        r#" \___ \  )(   ) )  ( (  ) )"#,
+        r#" (____/ (__) (___)  \____/ "#,
+        r#"                           "#,
+        r#"  SECURE TERRAFORM GATEWAY "#,
+        &format!("  v{} ", env!("CARGO_PKG_VERSION")),
     ];
 
-    let text = [
-        "  ",
-        "  ____  ____  _      ____  ",
-        " / ___)(_  _)( \\    / __ \\ ",
-        " \\___ \\  )(   ) )  ( (  ) )",
-        " (____/ (__) (___)  \\____/ ",
-        " ",
-        "  SECURE TERRAFORM GATEWAY ",
-        &format!("  v{} ", env!("CARGO_PKG_VERSION")),
+    let icon = [
+        r#"      .---------------.       "#,
+        r#"     /        |        \      "#,
+        r#"    |_________|_________|     "#,
+        r#"              |               "#,
+        r#"           ___#___            "#,
+        r#"          |   #   |           "#,
+        r#"          |   #   |           "#,
+        r#"          |___#___|           "#,
+        r#"     \________|________/      "#,
     ];
 
     println!();
     for i in 0..9 {
-        let left = logo[i];
-        let right = if i >= 1 && i - 1 < text.len() {
+        let left = if i >= 1 && i - 1 < text.len() {
             text[i - 1]
         } else {
             ""
         };
-        // Print with fixed width for the logo
-        println!("{}{}", style(format!("{: <22}", left)).cyan().bold(), style(right).cyan());
+        let right = icon[i];
+        
+        // Print text on left (cyan), icon on right (cyan bold)
+        println!("{}{}", style(format!("{: <30}", left)).cyan(), style(right).cyan().bold());
     }
     println!();
 }
