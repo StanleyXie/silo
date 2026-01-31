4b530dcdf8b0d7594694eb67c9b3be21326013c9
StanleyXie <stanley.xie@outlook.com>
2026-01-31 14:56:39 +0100

    feat: restore classic font style in banner #patch

--- src/banner.rs
+++ src/banner.rs
@@ -2,13 +2,14 @@ use console::style;
 
 pub fn print_banner() {
     let text = [
-        r#"  ____  ____  _      ____  "#,
-        r#" / ___)(_  _)( \    / __ \ "#,
-        r#" \___ \  )(   ) )  ( (  ) )"#,
-        r#" (____/ (__) (___)  \____/ "#,
-        r#"                           "#,
-        r#"  SECURE TERRAFORM GATEWAY "#,
-        &format!("  v{} ", env!("CARGO_PKG_VERSION")),
+        r#"  _____ _ _      "#,
+        r#" / ____(_) |     "#,
+        r#"| (___  _| | ___ "#,
+        r#" \___ \| | |/ _ \"#,
+        r#" ____) | | | (_) |"#,
+        r#"|_____/|_|_|\___/ "#,
+        r#"                  "#,
+        &format!("Secure Terraform State Gateway v{} ", env!("CARGO_PKG_VERSION")),
     ];
 
     let icon = [
@@ -25,15 +26,15 @@ pub fn print_banner() {
 
     println!();
     for i in 0..9 {
-        let left = if i >= 1 && i - 1 < text.len() {
-            text[i - 1]
+        let left = if i < text.len() {
+            text[i]
         } else {
             ""
         };
         let right = icon[i];
         
         // Print text on left (cyan), icon on right (cyan bold)
-        println!("{}{}", style(format!("{: <30}", left)).cyan(), style(right).cyan().bold());
+        println!("  {}{}", style(format!("{: <25}", left)).cyan(), style(right).cyan().bold());
     }
     println!();
 }
