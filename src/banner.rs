917b0e9deda0f064456b2d6a0bda4104b27a08e9
StanleyXie <stanley.xie@outlook.com>
2026-01-31 14:58:14 +0100

    feat: remove T-shape icon from banner #patch

--- src/banner.rs
+++ src/banner.rs
@@ -12,29 +12,9 @@ pub fn print_banner() {
         &format!("Secure Terraform State Gateway v{} ", env!("CARGO_PKG_VERSION")),
     ];
 
-    let icon = [
-        r#"   #######################   "#,
-        r#"   #######################   "#,
-        r#"            #####            "#,
-        r#"            #####            "#,
-        r#"            #####            "#,
-        r#"            #####            "#,
-        r#"            #####            "#,
-        r#"            #####            "#,
-        r#"            #####            "#,
-    ];
-
     println!();
-    for i in 0..9 {
-        let left = if i < text.len() {
-            text[i]
-        } else {
-            ""
-        };
-        let right = icon[i];
-        
-        // Ensure consistent width for text column
-        println!("  {}{}", style(format!("{: <45}", left)).cyan(), style(right).cyan().bold());
+    for line in text {
+        println!("  {}", style(line).cyan());
     }
     println!();
 }
